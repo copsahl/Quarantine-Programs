2146a34737856636e066166544eb725feb14b26d
Chase Opsahl <cjopsahl99@gmail.com>
2020-03-30 17:57:03 -0500

    Add files via upload

---
+++ compliment_generator/compliment_generator.py
@@ -0,0 +1,35 @@
+import socket
+import random
+from datetime import datetime
+
+sock = socket.socket()
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.bind(('',1337))
+
+with open("phrases.txt","r") as fobj:
+    phrases = fobj.readlines()
+
+log = open("log.txt", "a+")
+done = 0
+used = []
+now = datetime.now()
+
+while not done:
+    sock.listen(2)
+
+    client, addr = sock.accept()
+
+    print("Connection: {}\t{}".format(addr, now.strftime("%d/%m/%y %H:%M:%S")))
+    log.write("Connection: {}\t{}".format(addr, now.strftime("%d/%m/%y %H:%M:%S")))
+
+    phrase = random.choice(phrases)
+    while phrase in used:
+        phrase = random.choice(phrases)
+
+        if len(used) == len(phrases):
+            phrase = "Sorry, Anna. I am all out of phrases :(\nTher will be more!\nLove you <3"
+            done = 1
+
+    used.append(phrase)
+    client.send(phrase.encode())
+    client.close()
